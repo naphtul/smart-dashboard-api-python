a64ba03c986af8b509cf926e8ced8a96a34935e5
Naphtali Gilead <naphtul@gmail.com>
2023-06-26 18:55:40 -0400

    A working version of Kasa lights

--- src/controllers/InWalls.py
+++ src/controllers/InWalls.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, jsonify, Response
-from kasa_sdk_python import Kasa
+from kasa_sdk_python.Kasa import Kasa
 
 in_walls = Kasa()
 app = Blueprint('in_walls', __name__, url_prefix='/in_walls')
 
 
 @app.route('/', methods=['GET'])
-def get_devices() -> dict:
-    return in_walls.discover_devices()
+def get_devices() -> Response:
+    return jsonify(in_walls.discover())
 
 
 @app.route('/<device_name>', methods=['POST'])
